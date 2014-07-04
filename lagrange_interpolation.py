78e4de9ec4cdb850e64d1284389215fac6222448
Sam Manzer <samuelmanzer@users.noreply.github.com>
2014-07-04 11:28:21 -0700

    Bar chart working and everything nicely labeled

--- lagrange_interpolation.py
+++ lagrange_interpolation.py
@@ -46,12 +46,9 @@ def get_lagrange_poly(interp_points,fn_to_eval):
 # Plot the curves of the benchmark and the interpolating polynomials 
 # Returns MAE and RMSD of each poly
 def plot_lagrange_polys(x,lagrange_polys,y_exact,ax):
-    worst_points_x = []
-    worst_points_y = []
-
     mae_list = []
     rmsd_list = []
-
+    maxe_list = []
 
     for poly_idx in range(0,len(lagrange_polys)):
         # Plot
@@ -61,35 +58,39 @@ def plot_lagrange_polys(x,lagrange_polys,y_exact,ax):
         # Compute stats
         abs_errors = [abs(y[i] - y_exact[i]) for i in range(0,len(y))]
         max_error = max(abs_errors)
-        max_error_idx = abs_errors.index(max_error) 
-        worst_points_x.append(x[max_error_idx])
-        worst_points_y.append(y[max_error_idx])
         mae = sum(abs_errors)/float(len(abs_errors))
         rmsd = math.sqrt(sum(val*val for val in abs_errors)/float(len(abs_errors)))
         mae_list.append(mae)
         rmsd_list.append(rmsd)
-
+        maxe_list.append(max(abs_errors))
 
     # Plot exact result
     ax.plot(x,y_exact,label="cos(x)")
 
-    # Plot points where interpolation error is largest foreach degree 
-    ax.scatter(worst_points_x,worst_points_y,c="r")
-
-    return mae_list,rmsd_list
+    return mae_list,rmsd_list,maxe_list
 
 
 # Sets up a bar graph comparing the MAE and RMSD for different numbers
 # of interpolation points
-def plot_stats(mae_list,rmsd_list,n_points_range,ax):
-    ind = np.arange(1,len(mae_list)+1)
-    width = 0.5
+def plot_stats(mae_list,rmsd_list,maxe_list,n_points_range,ax):
+    ind = np.arange(1,3*len(mae_list)+1,3)
+    width = 0.75
     mae_rects = ax.bar(ind,mae_list,width,align="center",color='r')
-    rmsd_rects = ax.bar(ind+width,rmsd_list,width,align="center",color='y')
+    rmsd_rects = ax.bar(ind+width,rmsd_list,width,align="center",color='g')
+    maxe_rects = ax.bar(ind+width+width,maxe_list,width,align="center",color='b')
+    ticks = [ rect.get_x() for rect in rmsd_rects ]
+    ax.set_xticks(ticks)
+    ax.set_xticklabels([str(val) for val in n_points_range])
+    ax.set_xlabel("# of Interpolation Points")
+    ax.set_ylabel("Error Magnitude")
+    ax.legend(["MAE","RMSD","MAXE"],loc="upper right")
 
+
+    # Need to track these objects so we can adjust the axes to account for their size once
+    # figure is drawn
     texts = []
     rect_idx = 0
-    for rect in mae_rects+rmsd_rects:
+    for rect in mae_rects+rmsd_rects+maxe_rects:
         height = rect.get_height()
         t = ax.text(rect.get_x()+rect.get_width()/2,1.05*height,'%.2f' % height,ha='center')
         texts.append(t)
@@ -106,6 +107,8 @@ x = np.linspace(start,end,n_eval_pts)
 y_exact = np.cos(x)
 f1,(ax1,ax2) = plt.subplots(1,2,sharey=True)
 ax1.set_ylim(ymin=-1.1,ymax=1.5)
+ax1.set_title("Equally-Spaced")
+ax2.set_title("Chebyshev")
 
 f2,ax3 = plt.subplots()
 f3,ax4 = plt.subplots()
@@ -113,17 +116,18 @@ f3,ax4 = plt.subplots()
 # Equally spaced points
 evenly_spaced_sets = [np.linspace(start,end,n_points) for n_points in n_points_range]
 evenly_spaced_polys = [get_lagrange_poly(interp_points,math.cos) for interp_points in evenly_spaced_sets]
-mae_list,rmsd_list = plot_lagrange_polys(x,evenly_spaced_polys,y_exact,ax1)
-texts_1 = plot_stats(mae_list,rmsd_list,n_points_range,ax3)
+mae_list,rmsd_list,maxe_list = plot_lagrange_polys(x,evenly_spaced_polys,y_exact,ax1)
+texts_1 = plot_stats(mae_list,rmsd_list,maxe_list,n_points_range,ax3)
 
 # Chebyshev points - we must transform them to our interval
 cp_sets = [ [ math.cos((float(2*k - 1)/(2*n))*math.pi) for k in range(1,n+1)] for n in n_points_range ]
 tcp_sets = [ [ 0.5*((end - start)*pt + start + end) for pt in point_set] for point_set in cp_sets]
 chebyshev_point_polys = [get_lagrange_poly(interp_points,math.cos) for interp_points in tcp_sets]
-mae_list,rmsd_list = plot_lagrange_polys(x,chebyshev_point_polys,y_exact,ax2)
-texts_2 = plot_stats(mae_list,rmsd_list,n_points_range,ax4)
+mae_list,rmsd_list,maxe_list = plot_lagrange_polys(x,chebyshev_point_polys,y_exact,ax2)
+texts_2 = plot_stats(mae_list,rmsd_list,maxe_list,n_points_range,ax4)
 
-#f,(ax1,ax2) = plt.subplots(1,2,sharey=True)
+ax3.set_title("Lagrange Interpolation with Equally-Spaced Points")
+ax4.set_title("Lagrange Interpolation with Chebyshev Points")
 
 # Awful haxx for text labels above bars to not get cut off by top of figure
 tmp_file = tempfile.NamedTemporaryFile()
