1b0e10ab983f63f857ccb23e2a7708a487d64e3d
Sam Manzer <samuelmanzer@users.noreply.github.com>
2014-07-03 23:37:29 -0700

    Bar graph almost working - just needs MAXE and axes ticks tweaked

--- lagrange_interpolation.py
+++ lagrange_interpolation.py
@@ -9,6 +9,9 @@ from argparse import ArgumentParser
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import itertools
+import tempfile
+import pdb
 
 
 ###############################################################################
@@ -40,18 +43,32 @@ def get_lagrange_poly(interp_points,fn_to_eval):
         return val 
     return lagrange_poly 
 
+# Plot the curves of the benchmark and the interpolating polynomials 
+# Returns MAE and RMSD of each poly
 def plot_lagrange_polys(x,lagrange_polys,y_exact,ax):
     worst_points_x = []
     worst_points_y = []
 
+    mae_list = []
+    rmsd_list = []
+
 
     for poly_idx in range(0,len(lagrange_polys)):
+        # Plot
         y = [ lagrange_polys[poly_idx](pt) for pt in x ]
-        max_error = max(abs(y[i] - y_exact[i]) for i in range(0,len(x)))
-        max_error_idx = filter(lambda i:  abs(y[i] - y_exact[i]) == max_error,range(0,len(x)))[0]
+        ax.plot(x,y,label=str(n_points_range[poly_idx])+" Points")
+
+        # Compute stats
+        abs_errors = [abs(y[i] - y_exact[i]) for i in range(0,len(y))]
+        max_error = max(abs_errors)
+        max_error_idx = abs_errors.index(max_error) 
         worst_points_x.append(x[max_error_idx])
         worst_points_y.append(y[max_error_idx])
-        ax.plot(x,y,label=str(n_points_range[poly_idx])+" Points")
+        mae = sum(abs_errors)/float(len(abs_errors))
+        rmsd = math.sqrt(sum(val*val for val in abs_errors)/float(len(abs_errors)))
+        mae_list.append(mae)
+        rmsd_list.append(rmsd)
+
 
     # Plot exact result
     ax.plot(x,y_exact,label="cos(x)")
@@ -59,27 +76,70 @@ def plot_lagrange_polys(x,lagrange_polys,y_exact,ax):
     # Plot points where interpolation error is largest foreach degree 
     ax.scatter(worst_points_x,worst_points_y,c="r")
 
+    return mae_list,rmsd_list
+
+
+# Sets up a bar graph comparing the MAE and RMSD for different numbers
+# of interpolation points
+def plot_stats(mae_list,rmsd_list,n_points_range,ax):
+    ind = np.arange(1,len(mae_list)+1)
+    width = 0.5
+    mae_rects = ax.bar(ind,mae_list,width,align="center",color='r')
+    rmsd_rects = ax.bar(ind+width,rmsd_list,width,align="center",color='y')
+
+    texts = []
+    rect_idx = 0
+    for rect in mae_rects+rmsd_rects:
+        height = rect.get_height()
+        t = ax.text(rect.get_x()+rect.get_width()/2,1.05*height,'%.2f' % height,ha='center')
+        texts.append(t)
+        rect_idx += 1
+    return texts
+
+# Plot the statistica errors as a bar graph 
+
 n_eval_pts = 1000
 eval_step_size = float(end-start)/n_eval_pts
 n_points_range = range(2,6,1)
 
 x = np.linspace(start,end,n_eval_pts)
 y_exact = np.cos(x)
-f,(ax1,ax2) = plt.subplots(1,2,sharey=True)
+f1,(ax1,ax2) = plt.subplots(1,2,sharey=True)
 ax1.set_ylim(ymin=-1.1,ymax=1.5)
+
+f2,ax3 = plt.subplots()
+f3,ax4 = plt.subplots()
 
 # Equally spaced points
 evenly_spaced_sets = [np.linspace(start,end,n_points) for n_points in n_points_range]
 evenly_spaced_polys = [get_lagrange_poly(interp_points,math.cos) for interp_points in evenly_spaced_sets]
-plot_lagrange_polys(x,evenly_spaced_polys,y_exact,ax1)
-
-
+mae_list,rmsd_list = plot_lagrange_polys(x,evenly_spaced_polys,y_exact,ax1)
+texts_1 = plot_stats(mae_list,rmsd_list,n_points_range,ax3)
 
 # Chebyshev points - we must transform them to our interval
 cp_sets = [ [ math.cos((float(2*k - 1)/(2*n))*math.pi) for k in range(1,n+1)] for n in n_points_range ]
 tcp_sets = [ [ 0.5*((end - start)*pt + start + end) for pt in point_set] for point_set in cp_sets]
 chebyshev_point_polys = [get_lagrange_poly(interp_points,math.cos) for interp_points in tcp_sets]
-plot_lagrange_polys(x,chebyshev_point_polys,y_exact,ax2)
+mae_list,rmsd_list = plot_lagrange_polys(x,chebyshev_point_polys,y_exact,ax2)
+texts_2 = plot_stats(mae_list,rmsd_list,n_points_range,ax4)
+
+#f,(ax1,ax2) = plt.subplots(1,2,sharey=True)
+
+# Awful haxx for text labels above bars to not get cut off by top of figure
+tmp_file = tempfile.NamedTemporaryFile()
+f2.savefig(tmp_file.name)
+f3.savefig(tmp_file.name)
+renderer_2 = f2.axes[0].get_renderer_cache()
+renderer_3 = f3.axes[0].get_renderer_cache()
+for (ax,renderer,texts) in [(ax3,renderer_2,texts_1),(ax4,renderer_3,texts_2)]:
+    window_bbox_list = [t.get_window_extent(renderer) for t in texts]
+    data_bbox_list = [b.transformed(ax.transData.inverted()) for b in window_bbox_list]
+    data_coords_list = [b.extents for b in data_bbox_list] 
+    heights = [ coords[-1] for coords in data_coords_list]
+    #widths = [ coords[0] for coords in data_coords_list]
+    ax.set_ylim(ymax=max(heights)*1.05)
+    #ax.set_xlim(xmin=min(widths)*0.85)
+plt.show()
 
 #ax.legend(loc="lower right")
-f.savefig("lagrange_evenly_spaced.svg")
+#f.savefig("lagrange_evenly_spaced.svg")
